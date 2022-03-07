467198942ef1f86c68c9dad8e14889daf38820cd
C4rm3r <aha.aviv1@gmail.com>
2022-03-07 11:25:47 +0200

    Version #2

--- aviv.java
+++ aviv.java
@@ -2,8 +2,8 @@ public class aviv
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Version: #1");
-		// System.out.println("Version: #2");
+		// System.out.println("Version: #1");
+		System.out.println("Version: #2");
 		// System.out.println("Version: #3");
 		// System.out.println("Version: #4");
 		// System.out.println("Version: #5 <<< AND FINAL >>>");

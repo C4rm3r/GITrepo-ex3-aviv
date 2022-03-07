97038d52cd774a19a74ad2a64c031ed4ebefda60
C4rm3r <aha.aviv1@gmail.com>
2022-03-07 11:07:28 +0200

    Version #0 (Originsal File)

---
+++ aviv.java
@@ -0,0 +1,16 @@
+public class aviv
+{
+	public static void main(String[] args)
+	{
+		// System.out.println("Version: #1");
+		// System.out.println("Version: #2");
+		// System.out.println("Version: #3");
+		// System.out.println("Version: #4");
+		// System.out.println("Version: #5 <<< AND FINAL >>>");
+		
+		for (int i = 1; i <= 10; i++)
+		{
+			System.out.println("Aviv Aharon");
+		}
+	}
+}
